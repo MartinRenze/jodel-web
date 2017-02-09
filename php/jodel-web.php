0c8547e100f21c6db642b3e7d44804c468b2f14a
mmainstreet <martin.renze@gmx.de>
2017-02-09 19:28:33 +0100

    Add some Emoji

--- php/jodel-web.php
+++ php/jodel-web.php
@@ -504,29 +504,9 @@ function jodelToHtml($post, $view = 'time', $isDetailedView = FALSE)
 			<?php 
 			if(isset($post["image_url"]))
 			{
-				$text = $post["message"];
+			    $regexRest = '/[^\w$ .!?-]+/u';
 
-			    // Match Emoticons
-			    $regexEmoticons = '/[\x{1F600}-\x{1F64F}]/u';
-			    $clean_text = preg_replace($regexEmoticons, '', $text);
-
-			    // Match Miscellaneous Symbols and Pictographs
-			    $regexSymbols = '/[\x{1F300}-\x{1F5FF}]/u';
-			    $clean_text = preg_replace($regexSymbols, '', $clean_text);
-
-			    // Match Transport And Map Symbols
-			    $regexTransport = '/[\x{1F680}-\x{1F6FF}]/u';
-			    $clean_text = preg_replace($regexTransport, '', $clean_text);
-
-			    // Match Miscellaneous Symbols
-			    $regexMisc = '/[\x{2600}-\x{26FF}]/u';
-			    $clean_text = preg_replace($regexMisc, '', $clean_text);
-
-			    // Match Dingbats
-			    $regexDingbats = '/[\x{2700}-\x{27BF}]/u';
-			    $clean_text = preg_replace($regexDingbats, '', $clean_text);
-
-				echo '<img src="' . $post["image_url"] . '" alt="' . htmlspecialchars(preg_replace($regexEmoticons, '', $clean_text)) . '">';
+				echo '<img src="' . $post["image_url"] . '" alt="' . htmlspecialchars(preg_replace($regexRest, '', $post["message"])) . '">';
 			}
 			else {
 				echo str_replace('  ', ' &nbsp;', nl2br(htmlspecialchars($post["message"])));
